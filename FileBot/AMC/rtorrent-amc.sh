2b8b9a09aa25681a0b7045844e417a5cbd15cfb4
Xan <git@xanban.xyz>
2020-04-06 20:28:50 +0800

    added logfile

--- FileBot/AMC/rtorrent-amc.sh
+++ FileBot/AMC/rtorrent-amc.sh
@@ -15,6 +15,6 @@ MUSIC=y
 
 "$HOME"/bin/filebot -script fn:amc \
     --output "$HOME/media" \
-    -non-strict --encoding utf8 --log all --log-file amc-rtorrent.log --action symlink --conflict override \
+    -non-strict --encoding utf8 --log all --log-file "$HOME"/scripts/amc-rtorrent.log --action symlink --conflict override \
     --def artwork=false ut_kind=multi "ut_dir=$TORRENT_PATH" "ut_title=$TORRENT_NAME" subtitles=$SUBLANG \
         extractFolder="$HOME/files/_extracted" music=$MUSIC skipExtract=$SKIP_EXTRACT &
