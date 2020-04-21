6fe589d5a5fabf33e2029d6cd35c02664dec4e0b
Xan <git@xanban.xyz>
2020-04-21 15:46:56 +0800

    removed loops

--- MergerFS-Rclone/Service Files/Rclone Service File Check/normal-check.sh
+++ MergerFS-Rclone/Service Files/Rclone Service File Check/normal-check.sh
@@ -7,15 +7,12 @@
   echo "2) Show ExecStop"
   echo "3) Execute ExecStart"
   echo "4) Exit"
+  read -rp "Select an option: " option
 
-while true :
-do
-read -rp "Select an option: " option
 case $option in
   1) cat "$HOME"/.config/systemd/user/rclone-normal.service | sed -n '/^ExecStart=/,$p' | sed -n '/^ExecStop=/q;p' | sed 's/^ExecStart=//g' ;;
   2) cat "$HOME"/.config/systemd/user/rclone-normal.service | sed -n '/^ExecStop=/,$p' | sed -n '/^Restart=/q;p' | sed 's/^ExecStop=//g' ;;
   3) cat "$HOME"/.config/systemd/user/rclone-normal.service | sed -n '/^ExecStart=/,$p' | sed -n '/^ExecStop=/q;p' | sed 's/^ExecStart=//g' | bash ;;
   4) exit 0 ;;
   *) echo "Wrong option $option"
-  esac
-done
+esac
