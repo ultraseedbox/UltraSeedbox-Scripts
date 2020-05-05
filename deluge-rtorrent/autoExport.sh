08c7eac3bd10d0472d46f8792d3ea6cf50f2da69
Tyler Forest-Hauser <tyler@foresthauser.com>
2020-05-05 16:57:37 -0600

    Adds additional PYTHONPATH as fallback

--- deluge-rtorrent/autoExport.sh
+++ deluge-rtorrent/autoExport.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:$PYTHONPATH
+export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:/usr/lib/python3/dist-packages:$PYTHONPATH
 export PERL5LIB=$HOME/perl5/lib/perl5:$PERL5LIB
 
 /opt/deluge-1.3.15/bin/python2.7 $HOME/scripts/autoExport.py
