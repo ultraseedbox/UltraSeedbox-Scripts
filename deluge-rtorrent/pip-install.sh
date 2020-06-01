fdacd09eaa1d730defb176536b912e58bf2b871a
Xan <git@xanban.xyz>
2020-06-01 18:05:59 +0800

    Added script cleanup and changed instructions

--- deluge-rtorrent/pip-install.sh
+++ deluge-rtorrent/pip-install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
+# PIP Installer for USB Slots by Xan#7777
+# After installing, close your SSH and login again for profile to properly load.
+
 echo "Installing pip..."
-sleep 2
+
+# Running PIP Installer
 python3 <(curl https://bootstrap.pypa.io/get-pip.py) --user
-sed -i -e 's#PATH="$HOME/bin:$PATH"#PATH="$HOME/bin:$HOME/.local/bin:$PATH"#g' $HOME/.profile
-source $HOME/.profile
+
+# Replacing PATH
+sed -i -e 's|PATH="$HOME/bin:$PATH"|PATH="$HOME/bin:$HOME/.local/bin:$PATH"|g' "$HOME"/.profile
+
+# Echoes and Script Cleanup
 echo "Done."
-echo "Installer needs to logout your SSH session to properly apply the changes."
-echo "Please login to your slot's SSH again. Logging out..."
+echo "You need to close your SSH session and login into your SSH again after."
 sleep 5
+rm -- "$0"
 exit
