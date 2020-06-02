4fc5e74db7656cf520a757a75d13bf2bb82cb0cf
Xan <git@xanban.xyz>
2020-06-02 18:09:20 +0800

    script cleanup

--- FileBot/filebot-485-installer.sh
+++ FileBot/filebot-485-installer.sh
@@ -5,15 +5,15 @@
 # Includes OpenJDK 11 LTS Installer
 
 printf "\033[0;31mDisclaimer: This installer is unofficial and USB staff will not support any issues with it\033[0m\n"
-read -r -p "Type confirm if you wish to continue:" input
+read -r -p "Type confirm if you wish to continue: " input
 if [ ! "$input" = "confirm" ]
 then
     exit
 fi
 
+# Set FileBot version and URLs
 PACKAGE_VERSION=4.8.5
 PACKAGE_SHA256=$(curl -fsSL https://raw.githubusercontent.com/filebot/website/master/get.filebot.net/filebot/FileBot_$PACKAGE_VERSION/FileBot_$PACKAGE_VERSION-portable.tar.xz.sha256)
-
 PACKAGE_FILE=FileBot_$PACKAGE_VERSION-portable.tar.xz
 PACKAGE_URL=https://get.filebot.net/filebot/FileBot_$PACKAGE_VERSION/$PACKAGE_FILE
 
