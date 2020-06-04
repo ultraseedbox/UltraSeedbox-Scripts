51c25581749d5057942f8dfe436ee1ec1660e0a1
XantherBanter <git@xanban.xyz>
2020-06-04 16:14:43 +0200

    added shell restart

--- deluge-rtorrent/pip-install.sh
+++ deluge-rtorrent/pip-install.sh
@@ -11,9 +11,9 @@ python3 <(curl https://bootstrap.pypa.io/get-pip.py) --user
 # Replacing PATH
 sed -i -e 's|PATH="$HOME/bin:$PATH"|PATH="$HOME/bin:$HOME/.local/bin:$PATH"|g' "$HOME"/.profile
 
-# Echoes and Script Cleanup
-echo "Done."
-echo "You need to close your SSH session and login into your SSH again after."
-sleep 5
+# Restarting Shell
+exec $SHELL
+
+# Cleanup
 rm -- "$0"
 exit
