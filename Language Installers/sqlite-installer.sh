018217488436b5b553aa11546bb7a7803e7c078e
finch <57169808+fringillidaes@users.noreply.github.com>
2020-06-10 03:43:54 -0700

    Create sqlite-installer.sh

---
+++ Language Installers/sqlite-installer.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+printf "\033[0;31mDisclaimer: This installer is unofficial and USB staff will not support any issues with it\033[0m\n"
+read -r "Type confirm if you wish to continue: " input
+if [ ! "$input" = "confirm" ]
+then
+     exit
+fi
+
+if [ ! -f "$HOME"/bin/sqlite3 ]; then
+    mkdir -p "$HOME"/.sqlite-tmp
+    cd "$HOME"/.sqlite-tmp || exit
+    wget https://www.sqlite.org/2020/sqlite-autoconf-3320200.tar.gz
+    tar xvfz sqlite-autoconf-3320200.tar.gz
+    cd "$HOME"/.sqlite-tmp/sqlite-autoconf-3320200 || exit
+    ./configure --prefix="$HOME"
+    make
+    make install
+    cd "$HOME" || exit
+    rm -rfv "$HOME"/.sqlite-tmp
+    exit 0
+else
+    echo "Sqlite3 is already installed!"
+    exit 1
+fi
