10f21917bdaf80ef7392a13251759b45df70b4ba
XantherBanter <git@xanban.xyz>
2020-11-23 04:29:55 +0800

    corrected operator

--- MergerFS-Rclone/Installer Scripts/mergerfs-install.sh
+++ MergerFS-Rclone/Installer Scripts/mergerfs-install.sh
@@ -13,14 +13,14 @@ echo "1 = 2.28.3"
 echo "2 = latest (2.31)"
 read -r -p "Please select between 1 or 2: " mfs
 clear
-if [ "$mfs" == "1" ];
+if [ "$mfs" = "1" ];
 then
     echo "mergerfs is installing/upgrading..."
     mkdir -p "$HOME"/.mergerfs-tmp/
     wget https://github.com/trapexit/mergerfs/releases/download/2.28.3/mergerfs_2.28.3.debian-stretch_amd64.deb -O "$HOME"/.mergerfs-tmp/mergerfs.deb
     dpkg -x "$HOME"/.mergerfs-tmp/mergerfs.deb "$HOME"/.mergerfs-tmp
     cp "$HOME"/.mergerfs-tmp/usr/bin/* "$HOME"/bin
-elif [ "$mfs" == "2" ];
+elif [ "$mfs" = "2" ];
 then
     echo "mergerfs is installing/upgrading..."
     mkdir -p "$HOME"/.mergerfs-tmp/
