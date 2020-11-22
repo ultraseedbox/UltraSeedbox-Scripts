8bce5abb4fdb6ad41a68bcbed480694956f15f1e
XantherBanter <git@xanban.xyz>
2020-11-23 01:37:27 +0800

    update mergerfs installer

--- MergerFS-Rclone/Installer Scripts/mergerfs-install.sh
+++ MergerFS-Rclone/Installer Scripts/mergerfs-install.sh
@@ -6,11 +6,25 @@ if pgrep "mergerfs";
 then
     echo "mergerfs is running. Please close all mergerfs instances before proceeding."
     exit
-else
-    clear
+fi
+
+echo "Which version of mergerfs do you want to install?"
+echo "1 = 2.28.3"
+echo "2 = latest (2.31)"
+read -r -p "Please select between 1 or 2: " mfs
+clear
+if [ "$mfs" == "1" ];
+then
     echo "mergerfs is installing/upgrading..."
     mkdir -p "$HOME"/.mergerfs-tmp/
-    wget https://github.com/trapexit/mergerfs/releases/download/2.30.0/mergerfs_2.30.0.debian-stretch_amd64.deb -O "$HOME"/.mergerfs-tmp/mergerfs.deb
+    wget https://github.com/trapexit/mergerfs/releases/download/2.28.3/mergerfs_2.28.3.debian-stretch_amd64.deb -O "$HOME"/.mergerfs-tmp/mergerfs.deb
+    dpkg -x "$HOME"/.mergerfs-tmp/mergerfs.deb "$HOME"/.mergerfs-tmp
+    cp "$HOME"/.mergerfs-tmp/usr/bin/* "$HOME"/bin
+elif [ "$mfs" == "2" ];
+then
+    echo "mergerfs is installing/upgrading..."
+    mkdir -p "$HOME"/.mergerfs-tmp/
+    wget https://github.com/trapexit/mergerfs/releases/download/2.31.0/mergerfs_2.31.0.debian-stretch_amd64.deb -O "$HOME"/.mergerfs-tmp/mergerfs.deb
     dpkg -x "$HOME"/.mergerfs-tmp/mergerfs.deb "$HOME"/.mergerfs-tmp
     cp "$HOME"/.mergerfs-tmp/usr/bin/* "$HOME"/bin
 fi
