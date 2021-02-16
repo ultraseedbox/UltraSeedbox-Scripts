4297c6fd7004da76c64b79d2996b29c440b638e5
Tyler Forest-Hauser <tyler@foresthauser.com>
2021-02-16 11:56:13 -0700

    Adds FileBot v4.9.3
    • utilizes MINIMUM OpenJDK version (11.0.2) as it is quite a bit smaller
    • removes old version install instructions from README

--- FileBot/README.md
+++ FileBot/README.md
@@ -8,31 +8,25 @@ For the installers and the AMC scripts hosted here to work, you may have to do t
 
 ## Installation
 
-### Version 4.9.2 (Recommended)
+### Version 4.9.3 (Recommended)
 You can install this version using the instructions below:
 
-1. `wget https://raw.githubusercontent.com/ultraseedbox/UltraSeedbox-Scripts/master/FileBot/filebot-492-installer.sh`
-2. `chmod +x filebot-492-installer.sh`
-3. `./filebot-492installer.sh`
-4. `rm filebot-492-installer.sh`
+1. `wget https://raw.githubusercontent.com/ultraseedbox/UltraSeedbox-Scripts/master/FileBot/filebot-493-installer.sh`
+2. `chmod +x filebot-493-installer.sh` (no terminal output expected)
+3. `./filebot-493-installer.sh`
+4. `rm filebot-493-installer.sh`
 5. `filebot --license /path/to/FileBot_License_P1234567.psm`
-
-### Version 4.9.1
-You can install this version using the instructions below:
-
-1. `wget https://raw.githubusercontent.com/ultraseedbox/UltraSeedbox-Scripts/master/FileBot/filebot-491-installer.sh`
-2. `chmod +x filebot-491-installer.sh`
-3. `./filebot-491-installer.sh`
-4. `rm filebot-491-installer.sh`
-5. `filebot --license /path/to/FileBot_License_P1234567.psm`
-
-> To switch version, uninstall it first. This is to prevent any conflicts.
 
 ### AMC/ Torrent Client Post-processing
 
 If you're using FileBot for your torrent client post-processing, refer to the [AMC folder.](https://github.com/ultraseedbox/UltraSeedbox-Scripts/tree/master/FileBot/AMC)
 
 ## Uninstallation
+
+### Version 4.9.3
+
+1. `rm -rfv "$HOME"/filebot-493`
+2. `rm "$HOME"/bin/filebot`
 
 ### Version 4.9.2
 
---
+++ FileBot/filebot-493-installer.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Filebot 4.9.3 Installer by tylerforesthauser#9004 and Xan#7777
+# Based on the official tar installer: https://raw.githubusercontent.com/filebot/plugins/master/installer/tar.sh
+# Includes installer for OpenJDK 11.0.2
+
+printf "\033[0;31mDisclaimer: This installer is unofficial and USB staff will not support any issues with it\033[0m\n"
+read -r -p "Type confirm if you wish to continue: " input
+if [ ! "$input" = "confirm" ]
+then
+    exit
+fi
+
+# Set FileBot version and URLs
+PACKAGE_VERSION=4.9.3
+PACKAGE_SHA256=$(curl -fsSL https://raw.githubusercontent.com/filebot/website/master/get.filebot.net/filebot/FileBot_$PACKAGE_VERSION/FileBot_$PACKAGE_VERSION-portable.tar.xz.sha256)
+PACKAGE_FILE=FileBot_$PACKAGE_VERSION-portable.tar.xz
+PACKAGE_URL=https://get.filebot.net/filebot/FileBot_$PACKAGE_VERSION/$PACKAGE_FILE
+
+# Create directory for all FileBot data and change working directory
+mkdir -p "$HOME"/filebot-493 && cd "$HOME"/filebot-493 || exit
+
+# Fetch OpenJDK 11 binaries archive
+curl -o Java11.tar.gz "https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz"
+
+# Extract OpenJDK 11 binaries and remove archives
+tar xf Java11.tar.gz && rm Java11.tar.gz
+
+# Download FileBot package
+curl -o "$PACKAGE_FILE" -z "$PACKAGE_FILE" "$PACKAGE_URL"
+
+# Check SHA-256 checksum
+echo "$PACKAGE_SHA256 *$PACKAGE_FILE" | sha256sum --check || exit 1
+
+# Extract FileBot archive
+tar xf "$PACKAGE_FILE"
+
+# Clean up FileBot files
+rm "$PACKAGE_FILE" reinstall-filebot.sh update-filebot.sh
+
+# Increase maximum amount of memory that can be allocated to the JVM heap
+sed -i '/#!\/bin\/sh/a export JAVA_OPTS=\"-XX:CompressedClassSpaceSize=175m -XX:MaxMetaspaceSize=175m -XX:NativeMemoryTracking=summary -XX:MaxRAM=2g -XX:MaxRAMPercentage=70\"' filebot.sh
+
+# Use custom OpenJDK 15 installation to run FileBot
+sed -i '/^java/ s#java#'"$PWD"'\/jdk-11.0.2\/bin\/java#' filebot.sh
+
+# Check if filebot.sh works
+"$PWD/filebot.sh" -script https://raw.githubusercontent.com/ultraseedbox/UltraSeedbox-Scripts/master/FileBot/usbsysinfo.groovy
+
+# Link into user bin
+ln -sf "$PWD/filebot.sh" "$HOME"/bin/filebot
+
+# Check if the filebot command works and exit
+filebot -version
+exit
