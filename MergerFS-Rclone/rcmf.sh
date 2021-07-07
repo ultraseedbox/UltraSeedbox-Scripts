e346af323b321b7b2d7ad5e950516d34605678ab
Enon11 <piyushsinghal2@gmail.com>
2021-07-07 16:39:50 +0530

    Update rcmf.sh

--- MergerFS-Rclone/rcmf.sh
+++ MergerFS-Rclone/rcmf.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+OLDH=$1
+OLDU=$2
+OLD="/$1/$2"
 if test -f "$HOME/transfer/.config/systemd/user/rclone-vfs.service"; then
    cp $HOME/transfer/.config/systemd/user/rclone-vfs.service $HOME/.config/systemd/user/
-   sed -i 's/$HOME/%h/g' $HOME/.config/systemd/user/rclone-vfs.service
+   sed -i "s+$OLD+%h+g" $HOME/.config/systemd/user/rclone-vfs.service
    systemctl --user daemon-reload
    systemctl --user restart rclone-vfs.service
    echo "rclone-vfs service edited and restarted"
@@ -11,7 +13,7 @@ fi
 
 if test -f "$HOME/transfer/.config/systemd/user/mergerfs.service"; then
    cp $HOME/transfer/.config/systemd/user/mergerfs.service $HOME/.config/systemd/user/
-   sed -i 's/$HOME/%h/g' $HOME/.config/systemd/user/mergerfs.service
+   sed -i "s+$OLD+%h+g" $HOME/.config/systemd/user/mergerfs.service
    systemctl --user daemon-reload
    systemctl --user restart mergerfs.service
    echo "mergerfs service edited and restarted"
