106636747d7286c8b78240e123b6169124583e27
raikiri72 <82420828+raikiri72@users.noreply.github.com>
2021-07-09 18:10:12 +0530

    Update
    Added support for rclone-uploader

--- MergerFS-Rclone/rcmf.sh
+++ MergerFS-Rclone/rcmf.sh
@@ -3,6 +3,7 @@ if test -f "$HOME/transfer/.config/systemd/user/rclone-vfs.service"; then
    cp $HOME/transfer/.config/systemd/user/rclone-vfs.service $HOME/.config/systemd/user/
    sed -i -E "s+/home[0-9]{0,2}/\w*+%h+g" $HOME/.config/systemd/user/rclone-vfs.service
    systemctl --user daemon-reload
+   systemctl --user enable rclone-vfs.service
    systemctl --user restart rclone-vfs.service
    echo "rclone-vfs service edited and restarted"
 else echo "rclone-vfs.service not found in transfer folder"
@@ -12,7 +13,17 @@ if test -f "$HOME/transfer/.config/systemd/user/mergerfs.service"; then
    cp $HOME/transfer/.config/systemd/user/mergerfs.service $HOME/.config/systemd/user/
    sed -i -E "s+/home[0-9]{0,2}/\w*+%h+g" $HOME/.config/systemd/user/mergerfs.service
    systemctl --user daemon-reload
+   systemctl --user enable mergerfs.service
    systemctl --user restart mergerfs.service
    echo "mergerfs service edited and restarted"
 else echo "mergerfs.service not found in transfer folder"
+fi
+
+if test -f "$HOME/transfer/.config/systemd/user/rclone-uploader.service" && test -f "$HOME/transfer/.config/systemd/user/rclone-uploader.timer"; then
+   cp $HOME/transfer/.config/systemd/user/rclone-uploader.service $HOME/.config/systemd/user/ && cp $HOME/transfer/.config/systemd/user/rclone-uploader.timer $HOME/.config/systemd/user/
+   sed -i -E "s+/home[0-9]{0,2}/\w*+%h+g" $HOME/.config/systemd/user/rclone-uploader.service
+   systemctl --user daemon-reload
+   systemctl --user enable --now rclone-uploader.service && systemctl --user enable --now rclone-uploader.timer
+   echo "rclone-uploader service & timer edited and enabled"
+else echo "rclone-uploader or rclone-uploader.time not found in transfer folder"
 fi
