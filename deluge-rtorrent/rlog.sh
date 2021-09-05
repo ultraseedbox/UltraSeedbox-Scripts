9aae01c204615197f486ea826ac5c9ad9696189e
raikiri72 <82420828+raikiri72@users.noreply.github.com>
2021-09-05 17:03:23 +0530

    Update
    Added network logging.

--- deluge-rtorrent/rlog.sh
+++ deluge-rtorrent/rlog.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -d "$HOME/.config/rtorrent/logs"]
+then
+   echo "Rtorrent logging already enabled"
+   exit 0
+fi
+else
 app-rtorrent stop
 mkdir -p $HOME/.config/rtorrent/logs
 cp $HOME/.config/rtorrent/rtorrent.rc $HOME/.config/rtorrent/rtorrent.rc.bak
@@ -7,7 +13,8 @@ echo "#logs
 log.open_file = "rtorrent", ~/.config/rtorrent/logs/rtorrent.log
 log.open_file = "tracker", ~/.config/rtorrent/logs/tracker.log
 log.open_file = "storage", ~/.config/rtorrent/logs/storage.log
-    
+log.open_file = "network", ~/.config/rtorrent/logs/network.log   
+
 log.add_output = "info", "rtorrent"
 log.add_output = "critical", "rtorrent"
 log.add_output = "error", "rtorrent"
@@ -18,8 +25,19 @@ log.add_output = "debug", "rtorrent"
 log.add_output = "dht_debug", "tracker"
 log.add_output = "tracker_debug", "tracker"
   
-log.add_output = "storage_debug", "storage"" >> $HOME/.config/rtorrent/rtorrent.rc
+log.add_output = "storage_debug", "storage"
+
+log.add_output = "connection_bind", "network"
+log.add_output = "connection_fd", "network"
+log.add_output = "connection_filter", "network"
+log.add_output = "connection_hanshake", "network"
+log.add_output = "connection_listen", "network"
+log.add_output = "socket_error", "network"
+log.add_output = "socket_warn", "network"
+log.add_output = "socket_notice", "network"
+log.add_output = "socket_info", "network"
+log.add_output = "socket_debug", "network"" >> $HOME/.config/rtorrent/rtorrent.rc
 app-rtorrent restart
 sleep 2
 tail -f $HOME/.config/rtorrent/logs/rtorrent.log
-
+fi
