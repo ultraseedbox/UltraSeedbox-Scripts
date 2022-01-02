73ddcffc025d1d4e43376ad17939b892e653bd3d
raikiri72 <rohit.vardam@protonmail.com>
2022-01-02 23:36:43 +0530

    Added qBittorrent dark theme install script

---
+++ qBittorrent/qBitTheme.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+#Install check
+if [[ -f "$HOME"/.apps/nginx/prody.d/qbitdark.conf ]]; then
+echo "Already installed, exiting.."
+exit 0
+fi
+
+#Install theme
+app-nginx stop
+port=$(app-ports show| grep qBittorrent | awk '{print $1}')
+wget -P "$HOME/.apps/nginx/proxy.d/" https://raw.githubusercontent.com/raikiri72/UltraSeedbox-Scripts/qbitdarktheme/qBittorrent/qbitdark.conf
+sed -i "s/>port</$port/g" "$HOME"/.apps/nginx/proxy.d/qbitdark.conf
+app-nginx restart
+
+echo "qBittorrent dark theme installed, visit at following URL:https://$USER.$HOSTNAME.usbx.me/qbittorrent-dark"
+exit 0
