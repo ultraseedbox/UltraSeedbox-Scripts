fe3239aa9b16b9c1773d072f7f3d6d84ec776215
raikiri72 <rohit.vardam@protonmail.com>
2022-04-01 23:10:39 +0200

    Updated disclaimer

--- dotNetArrs/dotNetArrs-install.sh
+++ dotNetArrs/dotNetArrs-install.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 #Disclaimer
 
-printf "\033[0;31mDisclaimer: This installer is unofficial and USB staff will not support any issues with it\033[0m\n"
+printf "\033[0;31mDisclaimer: This installer is unofficial and Ultra.cc staff will not support any issues with it\033[0m\n"
 read -rp "Type confirm if you wish to continue: " input
 if [ ! "$input" = "confirm" ]; then
   exit
