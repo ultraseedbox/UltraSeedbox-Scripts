9662f76948a4f142d424a9bccab2d02867af64a7
raikiri72 <rohit.vardam@protonmail.com>
2022-05-09 15:29:41 +0200

    Added discord notification.

--- MergerFS-Rclone/rclone-mergerfs.sh
+++ MergerFS-Rclone/rclone-mergerfs.sh
@@ -7,8 +7,9 @@ set -euo pipefail
 printf "\033[0;31mDisclaimer: This installer is unofficial and Ultra.cc staff will not support any issues with it.\033[0m\n"
 read -rp "Type confirm if you wish to continue: " input
 if [ ! "$input" = "confirm" ]; then
-  exit
+    exit
 fi
+echo
 
 declare -a paths
 paths[1]="${HOME}/Stuff/Mount"
@@ -35,11 +36,36 @@ fi
 rclone selfupdate -q
 
 read -rp "Enter the rclone remote name for your Google Drive: " remote
+echo
 if ! rclone lsd "${remote}:" >>/dev/null; then
     echo "Configure your rclone remote correctly, then run the script again. https://docs.usbx.me/link/6#bkmrk-rclone"
     exit 1
 fi
 
+echo "Select type of rclone upload script [ Choose from 1 - 3 ]: "
+
+select upload in Normal Discord-Notification Quit; do
+    case ${upload} in
+    Normal)
+        upload="normal"
+        break
+        ;;
+    Discord-Notification)
+        upload="discord"
+        echo
+        read -rp "Enter the Discord Webhook URL: " webhook
+        break
+        ;;
+    Quit)
+        exit 0
+        ;;
+    *)
+        echo "Invalid option ${REPLY}"
+        ;;
+    esac
+done
+echo
+echo "Rclone-MergerFS workflow setup started.."
 for i in {1..5}; do
     if [ ! -d "${paths[${i}]}" ]; then
         mkdir -p "${paths[${i}]}"
@@ -102,12 +128,139 @@ systemctl --user daemon-reload
 systemctl --user enable --now --quiet rclone-vfs.service mergerfs.service
 sleep 5
 
-mdkir -p "${HOME}/MergerFS/Movies"
-mdkir -p "${HOME}/MergerFS/TV Shows"
+mkdir -p "${HOME}/MergerFS/Movies"
+mkdir -p "${HOME}/MergerFS/TV Shows"
 
-#Install rclone-upload.sh
+#Install rclone-upload script
 
-cat <<EOF | tee "${HOME}/scripts/rclone-upload.sh" >/dev/null
+if [ "${upload}" == "discord" ]; then
+    cat <<EOF | tee "${HOME}/scripts/rclone-upload.sh" >/dev/null
+#!/bin/bash
+
+# Rclone upload script with optional Discord notification upon move completion (if something is moved)
+#
+# Recommended for use via cron
+# For example: */10 * * * * /path/to/rclone-upload.sh >/dev/null 2>&1
+# -----------------------------------------------------------------------------
+
+SOURCE_DIR="\$HOME/Stuff/Local/"
+DESTINATION_DIR="${remote}:"
+
+DISCORD_WEBHOOK_URL="${webhook}"
+DISCORD_ICON_OVERRIDE="https://i.imgur.com/MZYwA1I.png"
+DISCORD_NAME_OVERRIDE="RCLONE"
+
+LOCK_FILE="\$HOME/rclone-upload.lock"
+LOG_FILE="\$HOME/rclone-upload.log"
+
+# DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING
+# -----------------------------------------------------------------------------
+
+trap 'rm -f \$LOCK_FILE; exit 0' SIGINT SIGTERM
+if [ -e "\$LOCK_FILE" ]
+then
+  echo "\$0 is already running."
+  exit
+else
+  touch "\$LOCK_FILE"
+  
+  rclone_move() {
+    rclone_command=\$(
+      "\$HOME"/bin/rclone move -vP \\
+      --config="\$HOME"/.config/rclone/rclone.conf \\
+      --exclude "Downloads/**" \\
+      --drive-chunk-size 64M \\
+      --use-mmap \\
+      --delete-empty-src-dirs \\
+      --log-file="\$LOG_FILE" \\
+      --stats=9999m \\
+      --tpslimit=5 \\
+      --transfers=2 \\
+      --checkers=4 \\
+      --bwlimit=8M \\
+      --drive-stop-on-upload-limit \\
+      "\$SOURCE_DIR" "\$DESTINATION_DIR" 2>&1
+    )
+    # "--stats=9999m" mitigates early stats output 
+    # "2>&1" ensures error output when running via command line
+    echo "\$rclone_command"
+  }
+  rclone_move
+
+  if [ "\$DISCORD_WEBHOOK_URL" != "" ]; then
+  
+    rclone_sani_command="\$(echo \$rclone_command | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g')" # Remove all escape sequences
+
+    # Notifications assume following rclone ouput: 
+    # Transferred: 0 / 0 Bytes, -, 0 Bytes/s, ETA - Errors: 0 Checks: 0 / 0, - Transferred: 0 / 0, - Elapsed time: 0.0s
+
+    transferred_amount=\${rclone_sani_command#*Transferred: }
+    transferred_amount=\${transferred_amount%% /*}
+    
+    send_notification() {
+      output_transferred_main=\${rclone_sani_command#*Transferred: }
+      output_transferred_main=\${output_transferred_main% Errors*}
+      output_errors=\${rclone_sani_command#*Errors: }
+      output_errors=\${output_errors% Checks*}
+      output_checks=\${rclone_sani_command#*Checks: }
+      output_checks=\${output_checks% Transferred*}
+      output_transferred=\${rclone_sani_command##*Transferred: }
+      output_transferred=\${output_transferred% Elapsed*}
+      output_elapsed=\${rclone_sani_command##*Elapsed time: }
+      
+      notification_data='{
+        "username": "'"\$DISCORD_NAME_OVERRIDE"'",
+        "avatar_url": "'"\$DISCORD_ICON_OVERRIDE"'",
+        "content": null,
+        "embeds": [
+          {
+            "title": "Rclone Upload Task: Success!",
+            "color": 4094126,
+            "fields": [
+              {
+                "name": "Transferred",
+                "value": "'"\$output_transferred_main"'"
+              },
+              {
+                "name": "Errors",
+                "value": "'"\$output_errors"'"
+              },
+              {
+                "name": "Checks",
+                "value": "'"\$output_checks"'"
+              },
+              {
+                "name": "Transferred",
+                "value": "'"\$output_transferred"'"
+              },
+              {
+                "name": "Elapsed time",
+                "value": "'"\$output_elapsed"'"
+              }
+            ],
+            "thumbnail": {
+              "url": null
+            }
+          }
+        ]
+      }'
+      
+      /usr/bin/curl -H "Content-Type: application/json" -d "\$notification_data" \$DISCORD_WEBHOOK_URL 
+    }
+    
+    if [ "\$transferred_amount" != "0" ]; then
+      send_notification
+    fi
+
+  fi
+
+  rm -f "\$LOCK_FILE"
+  trap - SIGINT SIGTERM
+  exit
+fi
+EOF
+else
+    cat <<EOF | tee "${HOME}/scripts/rclone-upload.sh" >/dev/null
 #!/bin/bash
 
 lock_file="\$HOME/scripts/rclone-upload.lock"
@@ -138,13 +291,17 @@ else
     exit
 fi
 EOF
+fi
 
 chmod +x "${HOME}/scripts/rclone-upload.sh"
 
 #Install rclone-upload cronjob
 
-croncmd="$HOME/scripts/rclone-upload.sh > /dev/null 2>&1"
+croncmd="${HOME}/scripts/rclone-upload.sh > /dev/null 2>&1"
 cronjob="0 19 * * * $croncmd"
-( crontab -l | grep -v -F "$croncmd" || : ; echo "$cronjob" ) | crontab -
+(
+    crontab -l | grep -v -F "$croncmd" || :
+    echo "$cronjob"
+) | crontab -
 
 echo "Work flow set up complete. Continue following the guide."
