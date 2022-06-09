9a7bafc1b782af4603a72229e7efc5c2c193c7e8
raikiri72 <rohit.vardam@protonmail.com>
2022-06-09 14:36:51 +0200

    Updated extractFolder

--- FileBot/AMC/rtorrent-amc.sh
+++ FileBot/AMC/rtorrent-amc.sh
@@ -17,4 +17,4 @@ MUSIC=y
     --output "$HOME/media" \
     -non-strict --encoding utf8 --log all --log-file "$HOME"/scripts/amc-rtorrent.log --action symlink --conflict override \
     --def artwork=false ut_kind=multi "ut_dir=$TORRENT_PATH" "ut_title=$TORRENT_NAME" subtitles=$SUBLANG \
-        extractFolder="$HOME/files/_extracted" music=$MUSIC skipExtract=$SKIP_EXTRACT &
+        extractFolder="$HOME/downloads/_extracted" music=$MUSIC skipExtract=$SKIP_EXTRACT &
