da77bfbc586f8bddfdb8c1e438dddad446981a19
raikiri72 <82420828+raikiri72@users.noreply.github.com>
2022-12-12 17:30:27 +0530

    Update mergerfs-install.sh

--- MergerFS-Rclone/Installer Scripts/mergerfs-install.sh
+++ MergerFS-Rclone/Installer Scripts/mergerfs-install.sh
@@ -11,12 +11,14 @@ clear
 
 lsb_release -d | grep -q 10 && debversion="buster" || debversion="stretch"
 
+default_version="2.33.5"
+
 echo "Select version of mergerfs to install. [ Choose from 1 - 3 ]: "
 
-select version in 2.31.0 Latest Quit; do
+select version in ${default_version} Latest Quit; do
     case ${version} in
-    2.31.0)
-        version="2.31.0"
+    $default_version)
+        version="${default_version}"
         break
         ;;
     Latest)
@@ -34,17 +36,21 @@ select version in 2.31.0 Latest Quit; do
     esac
 done
 clear
-if [ "${version}" = "2.31.0" ]; then
+if [ "${version}" = "${default_version}" ]; then
     echo "mergerfs is installing/upgrading..."
     mkdir -p "$HOME"/.mergerfs-tmp/
-    wget "https://github.com/trapexit/mergerfs/releases/download/2.31.0/mergerfs_2.31.0.debian-${debversion}_amd64.deb" -O "$HOME"/.mergerfs-tmp/mergerfs.deb
+    wget "https://github.com/trapexit/mergerfs/releases/download/${version}/mergerfs_${version}.debian-${debversion}_amd64.deb" -O "$HOME"/.mergerfs-tmp/mergerfs.deb
     rm -rf "$HOME"/bin/*mergerfs*
     dpkg -x "$HOME"/.mergerfs-tmp/mergerfs.deb "$HOME"/.mergerfs-tmp
     cp "$HOME"/.mergerfs-tmp/usr/bin/* "$HOME"/bin
 else
     echo "mergerfs is installing/upgrading..."
     mkdir -p "$HOME"/.mergerfs-tmp/
-    wget "https://github.com/trapexit/mergerfs/releases/download/${version}/mergerfs_${version}.debian-${debversion}_amd64.deb" -O "$HOME"/.mergerfs-tmp/mergerfs.deb
+    wget "https://github.com/trapexit/mergerfs/releases/download/${version}/mergerfs_${version}.debian-${debversion}_amd64.deb" -O "$HOME"/.mergerfs-tmp/mergerfs.deb || {
+        echo "Asset for mergerfs version ${version} not found. Try installing ${default_version} by running the script again."
+        rm -rf "$HOME"/.mergerfs-tmp
+        exit 1
+    }
     rm -rf "$HOME"/bin/*mergerfs*
     dpkg -x "$HOME"/.mergerfs-tmp/mergerfs.deb "$HOME"/.mergerfs-tmp
     cp "$HOME"/.mergerfs-tmp/usr/bin/* "$HOME"/bin
@@ -55,11 +61,11 @@ clear
 if [[ $("$HOME"/bin/mergerfs -V) ]]; then
     echo "MergerFS installed correctly! Restart your SSH session to properly apply changes!"
     rm -rf "$HOME"/.mergerfs-tmp
-    exit 1
+    exit 0
 else
     echo "$version"
     echo "mergerfs install somehow failed. Please run this again!"
     rm -rf "$HOME"/.mergerfs-tmp
-    exit 2
+    exit 1
 fi
 exit
